0b9c49c9b06841393aebbed8dabc906929dc3e9c
Panos <panosynt@gmail.com>
2026-03-16 11:44:56 +0200

    fix ledger package name in ledger boot module

--- ledger/ledger-boot/src/main/java/com/hexagonal/server/ledger/HexagonalServerLedgerApplication.java
+++ ledger/ledger-boot/src/main/java/com/hexagonal/server/ledger/HexagonalServerLedgerApplication.java
@@ -1,4 +1,4 @@
-package com.hexagonal.server.identity;
+package com.hexagonal.server.ledger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
